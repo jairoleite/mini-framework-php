2fdcc75a3bf261eb86707b42aef745c995690438
jairo.leite <jairoleite@jairo.leite>
2019-03-31 14:33:39 -0300

    projeto de mini framework para aplicações modulares

--- templates/404.php
+++ templates/404.php
@@ -3,32 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">  <link rel="stylesheet" href="assets/css/not-found.css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+  <link rel="stylesheet" href="assets/css/not-found.css">
   <title>página não encontrada</title>
 </head>
-
-<style>
-   body {
-    background-color: #c2deef; 
-   }
-
-   div {
-     display: flex;
-     flex-direction: column;
-     justify-content: center;
-     align-items: center;
-   }
-
-   h1 {
-     color: #000;
-     font-size: 40px;
-   }
-</style>
-
 <body>
   <div>
   <img src="assets/images/404.png" width="50%" alt="image">
-  <h1>Desculpe essa página não existe...</h1>
+  <h1>Desculpe essa página  que você digitou não existe...</h1>
   </div> 
 </body>
 </html>
